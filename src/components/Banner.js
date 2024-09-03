7a1c88cc7fbf8959c8bef5d4b7c91586778174c0
Your Name <Boyandlovu@gmail.com>
2024-09-03 16:04:55 +0200

    changed code

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,14 +4,11 @@ import "./Banner.css";
 
 
 const Banner = () => {
- 
   return (
     <div className="banner">
-      <div classname="banner-info">
+      <div className="banner-info">
         <h1>Get out and stretch your imagination</h1>
-        <h5>
-          Plan a different kind of gateway to uncover the hidden gems near you
-        </h5>
+        <h5>Plan a different kind of gateway to uncover the hidden gems near you</h5>
         <Button variant="outlined">Explore Nearby</Button>
       </div>
       <img
